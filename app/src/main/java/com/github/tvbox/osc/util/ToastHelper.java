bd49f8bb9ee1ae79205b9d9558b068f9ea061d56
zouyq <zyqcn@live.com>
2026-01-16 16:27:55 +0800

    feat: update AndroidManifest and ToastHelper for improved functionality
    - Added a new library to the overrideLibrary list in AndroidManifest.xml.
    - Introduced a new method in ToastHelper to display Toast messages using the Application context.
    - Added a new dimension value (ts_28) in dimens.xml for layout adjustments.

--- app/src/main/java/com/github/tvbox/osc/util/ToastHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/ToastHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.widget.Toast;
 import android.os.Looper;
 
+import com.github.tvbox.osc.base.App;
+
 public class ToastHelper {
 
     public static void showToast(Context context, String text) {
@@ -14,6 +16,16 @@ public class ToastHelper {
                 Looper.loop();
             }
         }).start();
+    }
+
+    /**
+     * 使用 Application context 显示 Toast
+     */
+    public static void show(String text) {
+        App app = App.getInstance();
+        if (app != null) {
+            showToast(app, text);
+        }
     }
 
     public static void debugToast(Context context, String text) {
